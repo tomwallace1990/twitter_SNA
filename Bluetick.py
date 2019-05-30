5038c09b02a0b2956082e6e17547130a0cef31d8
Tom Wallace <31849043+tomwallace1990@users.noreply.github.com>
2019-05-30 14:30:36 +0100

    Updated bluetick
    Can also check if sender is blueticked from tweet object

--- Bluetick.py
+++ Bluetick.py
@@ -18,9 +18,24 @@ def athenticate(tokenpath):
 	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 	return api
 
+def searchtwit(query, max_tweets):
+	
+	searched_tweets = [status for status in tweepy.Cursor(api.search, q=query, lang='en').items(max_tweets)]
+	tweet_author = [tweet.user.screen_name for tweet in searched_tweets]
+	tweet_text = [tweet.text for tweet in searched_tweets]
+
+	return tweet_author, tweet_text, searched_tweets
+
+##############################
+
 tokenpath = 'C:/Users/Tom Wallace/Dropbox/Stats/twitter_tokens/twitter-api-token_nyetarussian.json'
 api = athenticate(tokenpath) # auth
 
 user = api.get_user(screen_name='David_Cameron')
 
 print(user.verified)
+
+max_tweets = 3
+query = 'Extinction rebellion'
+account, corpus, searched_tweets = searchtwit(query, max_tweets)
+print(searched_tweets[0].user.verified) # can get the status of the sender, but not the mentioned accounts - if have to get the revicer list seperatly (inneficent but could cross check against the sender list first)
